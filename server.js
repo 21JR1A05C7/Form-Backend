7e7aa5de8c628acd395970f759a5c58a0f3e6486
Mahendra Nath Ponduri <140336191+21JR1A05C7@users.noreply.github.com>
2024-07-18 21:25:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,30 @@
 //     console.log(`Server is running on port: ${port}`);
 // });
 
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => console.log('MongoDB database connection established successfully'))
+//     .catch(err => console.error('MongoDB connection error:', err));
+
+// const formRouter = require('./routes/form');
+// app.use('/form', formRouter);
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port: ${port}`);
+// });
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -50,3 +74,4 @@ app.use('/form', formRouter);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
