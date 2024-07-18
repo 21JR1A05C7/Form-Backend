95c41a9e669ce0760da47a874b60970fa2834043
Mahendra Nath Ponduri <140336191+21JR1A05C7@users.noreply.github.com>
2024-07-18 21:27:58 +0530

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -1,9 +1,29 @@
+// const router = require('express').Router();
+// let Form = require('../models/form.model');
+
+// router.route('/add').post((req, res) => {
+//     const fullName = req.body.fullName;
+//     const email = req.body.email;
+
+//     const newForm = new Form({ fullName, email });
+
+//     newForm.save()
+//         .then(() => res.json('Form added!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// module.exports = router;
+
 const router = require('express').Router();
 let Form = require('../models/form.model');
 
 router.route('/add').post((req, res) => {
     const fullName = req.body.fullName;
     const email = req.body.email;
+
+    if (!fullName || !email) {
+        return res.status(400).json('Error: Missing required fields');
+    }
 
     const newForm = new Form({ fullName, email });
 
@@ -13,3 +33,4 @@ router.route('/add').post((req, res) => {
 });
 
 module.exports = router;
+
